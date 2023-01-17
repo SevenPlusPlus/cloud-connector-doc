bce22593b77762ee86affe10f8e3f4274b4b1328
wangqijia <wangqijia@youzan.com>
2023-01-17 20:16:14 +0800

    new version

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,10 +1,10 @@
 !/bin/bash
 
 echo "remove old doc dir"
-rm -rf ./doc
+rm -rf ./docs
 
 echo "generate new docs"
-javadoc -d ./doc -encoding utf-8 -charset utf-8 -private -sourcepath /Users/sevenplus/workspace/cloud-connector-sdk/cloud-connector-sdk-core/src/main/java -subpackages com
+javadoc -d ./docs -encoding utf-8 -charset utf-8 -private -sourcepath /Users/sevenplus/workspace/cloud-connector-sdk/cloud-connector-sdk-core/src/main/java -subpackages com
 
 echo "commit change"
 git commit -m "new version" -a
