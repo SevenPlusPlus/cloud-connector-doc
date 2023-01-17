d77dad9d144239089bc42657bd5a8bec9be7c023
wangqijia <wangqijia@youzan.com>
2023-01-17 20:08:33 +0800

    add script

--- generate-doc.sh
+++ generate-doc.sh
@@ -5,3 +5,9 @@ rm -rf ./doc
 
 echo "generate new docs"
 javadoc -d ./doc -encoding utf-8 -charset utf-8 -private -sourcepath /Users/sevenplus/workspace/cloud-connector-sdk/cloud-connector-sdk-core/src/main/java -subpackages com
+
+echo "commit change"
+git commit -m "new version" -a
+
+echo "push to remote repository"
+git push origin gh-pages
